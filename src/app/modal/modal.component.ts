5a96f81bd5fb3e85684bc1819d892bbcbf1aa022
Giyeon Kim <gompu123@gmail.com>
2018-05-29 14:07:02 +0900

    modal description

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { RBTree } from "../models";
+import { Color, RBTree } from "../models";
 
 @Component({
   selector: 'modal',
@@ -8,13 +8,22 @@ import { RBTree } from "../models";
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
-  currentStage: number = 3
-  rbTree: RBTree = new RBTree([12, 5, 15, 3, 10, 13, 17, 4, 7, 11, 14, 6, 8])
+  currentStage: number = 1
+  rbTree: RBTree = new RBTree([12, 5, 15, 3, 10, 13, 17, 4, 7, 11, 14])
+  rbTree1: RBTree = new RBTree([1, 2, 3])
+  rbTree3: RBTree = new RBTree([1, 2, 3, 4, 5, 6, 7])
+  Color = Color
   // rbTree: RBTree = new RBTree()
 
   constructor(private modalService: NgbModal) {}
 
   ngOnInit() {
+    this.rbTree1.root.color = Color.RED
+    const node3 = this.rbTree3.root.right.right
+    node3.color = Color.RED
+    node3.left.color = Color.BLACK
+    node3.right.color = Color.BLACK
+    // this.rbTree1.
   }
 
   open(content) {
